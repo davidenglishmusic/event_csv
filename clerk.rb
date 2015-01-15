77a960b12d473ecf3ff83b1170bfd6107f11d818
David English <david@operaonfilm.com>
2015-01-14 20:29:55 -0800

    Removed several Rubocop offenses

--- clerk.rb
+++ clerk.rb
@@ -1,9 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
 
-class CSV_Clerk
+class Clerk
 
-  CALENDAR_SELECTOR = ".calendar-wrap"
+  CALENDAR_SELECTOR = '.calendar-wrap'
   ARTICLE_SELECTOR = 'article'
   EVENT_TITLE_SELECTOR = '.event-info.title'
   DATE_SELECTOR = '.date'
@@ -25,7 +25,7 @@ class CSV_Clerk
   end
 
   def build_csv
-    csv = ""
+    csv = ''
     csv << CSV_OPENING_LINE
     @calendar.css(ARTICLE_SELECTOR).each do |event|
       csv << get_event_info(event)
@@ -36,22 +36,22 @@ class CSV_Clerk
   private
 
   def get_event_info(event)
-    info = ""
-    info << event.css(EVENT_TITLE_SELECTOR).to_s.scan(/<p class="event-info title">(.*?)</).join + ","
-    info << cleanse_of_commas(event.css(DATE_SELECTOR)[0].to_s.scan(/,(.*?)</).join) + ","
-    info << event.css(TIME_SELECTOR)[0].to_s.scan(/<span class="time">(.*?)</).join + ","
-    info << cleanse_of_HTML_code(event.css(LOCATION_NAME_SELECTOR).to_s.to_s.scan(/<span class="fn org">(.*?)</).join) + " - "
-    info << event.css(ADDRESS_SELECTOR).to_s.scan(/<span class="address">(.*?)</).join + " "
-    info << event.css('.city').to_s.scan(/<span class="city">(.*?)</).join + ","
-    if (is_buying_option(event.css(EXTRA_INFORMATION_SELECTOR)[4].to_s))
-      info << " "
+    info = ''
+    info << event.css(EVENT_TITLE_SELECTOR).to_s.scan(/<p class="event-info title">(.*?)</).join + ','
+    info << cleanse_of_commas(event.css(DATE_SELECTOR)[0].to_s.scan(/,(.*?)</).join) + ','
+    info << event.css(TIME_SELECTOR)[0].to_s.scan(/<span class="time">(.*?)</).join + ','
+    info << cleanse_of_html_code(event.css(LOCATION_NAME_SELECTOR).to_s.to_s.scan(/<span class="fn org">(.*?)</).join) + ' - '
+    info << event.css(ADDRESS_SELECTOR).to_s.scan(/<span class="address">(.*?)</).join + ' '
+    info << event.css('.city').to_s.scan(/<span class="city">(.*?)</).join + ','
+    if buying_option?(event.css(EXTRA_INFORMATION_SELECTOR)[4].to_s)
+      info << ' '
     else
       info << event.css('p')[4].to_s.scan(/<p>(.*?)</).join
     end
     info << "\n"
   end
 
-  def is_buying_option(element)
+  def buying_option?(element)
     if element.to_s == '<p class="event-info buying-options">
 
       </p>'
@@ -65,8 +65,7 @@ class CSV_Clerk
     element.sub(',', '')
   end
 
-  def cleanse_of_HTML_code(element)
+  def cleanse_of_html_code(element)
     element.sub('&amp;', '&').sub('<br />', ' - ')
   end
-
 end
--- event_csv.rb
+++ event_csv.rb
@@ -1,8 +1,8 @@
 require_relative 'clerk'
 
-puts "Enter the URL to be scraped"
+puts 'Enter the URL to be scraped'
 url = gets.chomp
 
-clerk = CSV_Clerk.new(url)
+clerk = Clerk.new(url)
 
 File.open('events.csv', 'w') { |file| file.write(clerk.build_csv) }
